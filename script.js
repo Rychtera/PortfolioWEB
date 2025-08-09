e6ec8e7ebc414cdec5d6b6ae22bd9677b2a34677
Rychtera <rychtera.creator@gmail.com>
2025-08-09 23:25:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function setLang(lang) {
     el.textContent = el.dataset[lang];
   });
 }
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const closeBtn = document.getElementById("closeBtn");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
