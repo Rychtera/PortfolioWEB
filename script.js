5264a1ba395722a302e49a72eb64428e05ab7ae0
Rychtera <rychtera.creator@gmail.com>
2025-08-13 16:13:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ document.querySelectorAll(".gallery img").forEach(img => {
 closeBtn.addEventListener("click", () => {
   lightbox.style.display = "none";
 });
+
+
+const hoverArea = document.getElementById('hover-area');
+const tooltip = document.getElementById('tooltip');
+
+hoverArea.addEventListener('mousemove', (e) => {
+  tooltip.style.display = 'block';
+  tooltip.style.left = e.pageX + 10 + 'px'; // offset from cursor
+  tooltip.style.top = e.pageY + 10 + 'px';
+});
+
+hoverArea.addEventListener('mouseleave', () => {
+  tooltip.style.display = 'none';
+});
